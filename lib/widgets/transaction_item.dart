f6dae68846e11456fc6004a390c62216030cac78
ArerehRaj <raj.jr@somaiya.edu>
2021-10-13 00:18:49 +0530

    Added dismissile function to transaction item card

--- lib/widgets/transaction_item.dart
+++ lib/widgets/transaction_item.dart
@@ -12,26 +12,50 @@ class TransactionItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final deviceSize = MediaQuery.of(context).size;
-    return Card(
-      elevation: 5,
-      margin: const EdgeInsets.symmetric(
-        vertical: 8,
-        horizontal: 5,
+    return Dismissible(
+      key: ValueKey(transaction.id),
+      background: Container(
+        color: Theme.of(context).errorColor.withOpacity(0.25),
+        child: const Icon(
+          Icons.delete,
+          color: Colors.white,
+          size: 40,
+        ),
+        alignment: Alignment.centerRight,
+        padding: const EdgeInsets.only(right: 20),
+        margin: const EdgeInsets.symmetric(
+          horizontal: 15,
+          vertical: 8,
+        ),
       ),
-      child: ListTile(
-        leading: CircleAvatar(
-          radius: 30,
-          backgroundColor: Theme.of(context).primaryColor,
-          child: Padding(
-            padding: const EdgeInsets.all(6),
-            child: FittedBox(
-              child: Text('₹${transaction.amount}'),
+      direction: DismissDirection.endToStart,
+      onDismissed: (direction) {
+        print('Removing');
+      },
+      confirmDismiss: (direction) {
+        return Future.value(true);
+      },
+      child: Card(
+        elevation: 5,
+        margin: const EdgeInsets.symmetric(
+          vertical: 8,
+          horizontal: 5,
+        ),
+        child: ListTile(
+          leading: CircleAvatar(
+            radius: 30,
+            backgroundColor: Theme.of(context).primaryColor,
+            child: Padding(
+              padding: const EdgeInsets.all(6),
+              child: FittedBox(
+                child: Text('₹${transaction.amount}'),
+              ),
             ),
           ),
-        ),
-        title: Text(transaction.title),
-        subtitle: Text(
-          DateFormat.yMMMd().format(transaction.date),
+          title: Text(transaction.title),
+          subtitle: Text(
+            DateFormat.yMMMd().format(transaction.date),
+          ),
         ),
       ),
     );
